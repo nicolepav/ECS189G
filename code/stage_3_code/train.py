bbcb9ab0cfa5bdccaac04f592c1f17cce689d2c4
Leila-Jadoon <leilajadoon@gmail.com>
2022-02-13 22:06:20 -0800

    CIFAR with modified lenet-5

--- code/stage_3_code/train.py
+++ code/stage_3_code/train.py
@@ -5,6 +5,7 @@ TBD
 # Copyright (c) 2022-Current Jialiang Wang <jilwang804@gmail.com>
 # License: TBD
 
+from sklearn import datasets
 from src.dataset_loader import Dataset_Loader
 from src.model import CNN
 from src.result_saver import Result_Saver
@@ -13,6 +14,23 @@ import numpy as np
 import sys
 import torch
 from torch import nn
+
+# import pickle
+# import pandas as pd
+
+# # ---- View dataset ------------------------------
+# if 1:
+#     f = open('datasets/stage_3_data/CIFAR', 'rb')
+    
+#     data = pickle.load(f)
+#     f.close()
+
+#     print('training set size:', len(data['train']), 'testing set size:', len(data['test']))
+#     for pair in data['train']:
+#    # for pair in data['test']:
+#        plt.imshow(pair['image'], cmap="Greys")
+#        plt.show()
+#        print(pair['label'])
 
 
 #---- Convolutional Neural Network Script ----
@@ -40,44 +58,31 @@ if 1:
     # Default setting for CIFAR dataset
     in_channels = 3             # 1 for grayscale, 3 for color RGB
     num_classes = 10
-    layers_data = [             # Baseline: AlexNet
+    layers_data = [             # Implements Lenet-5
         # Layer 1       32 * 32 * 3
-        nn.Conv2d(in_channels, 6, kernel_size=3, stride=1, padding=1, dilation=1),  # (32 + 2 - 3) / 1 + 1 = 32
+        nn.Conv2d(in_channels, out_channels=6, kernel_size=5, stride=1, padding=0, dilation=1),  # (32 - 5) / 1 + 1 = 28
         nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (32 - 2) / 2 + 1 = 16
+        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (28 - 2) / 2 + 1 = 14
 
-        # Layer 2       16 * 16 * 6
-        nn.Conv2d(6, 16, kernel_size=3, stride=1, padding=1, dilation=1),           # (16 - 3 + 2) / 1 + 1 = 16
+        # Layer 2       14 * 14 * 6
+        nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0, dilation=1),           # (14 - 5) / 1 + 1 = 10
         nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (16 - 2) / 2 + 1 = 8
+        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (10 - 2) / 2 + 1 = 5
 
-        # Layer 3       8 * 8 * 16
-        nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, dilation=1),          # (8 - 3 + 2) / 1 + 1 = 8
+        # Layer 3       5 * 5 * 16
+        nn.Conv2d(16, 120, kernel_size=5, stride=1, padding=0, dilation=1),          # (5 - 5) / 1 + 1 = 1
         nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (8 - 2) / 2 + 1 = 4
-
-        # Layer 4       4 * 4 * 64
-        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, dilation=1),          # (4 - 3 + 2) / 1 + 1 = 4
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (4 - 2) / 2 + 1 = 2
-
-        # Layer 5       2 * 2 * 128
-        nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, dilation=1),         # (2 - 3 + 2) / 1 + 1 = 2
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2, padding=0),                           # (2 - 2) / 2 + 1 = 1
 
         # fc Layer (dense layer)
         # Before the dense layer, need to flatten all dimensions except batch
         # This is down in the model.py (keep in mind)
-        nn.Linear(128, 120),
-        nn.ReLU(),
         nn.Linear(120, 84),
         nn.ReLU(),
-        nn.Linear(84, num_classes)
+        nn.Linear(84, 10)
     ]
 
     # Usage: CNN(layers_data, learning_rate, epoch, batch, optimizer, loss_function, device)
-    method_obj = CNN(layers_data, 1e-3, 60, 32, torch.optim.Adam, nn.CrossEntropyLoss(), device)
+    method_obj = CNN(layers_data, 1e-3, 30, 256, torch.optim.Adam, nn.CrossEntropyLoss(), device)
 
     result_obj = Result_Saver('Model saver', '')
     result_obj.result_destination_file_path = 'result/CNN_model.pth'
@@ -87,9 +92,9 @@ if 1:
 
     # ---- Running Section ---------------------------------
     print('************ Start ************')
-    X_train = torch.FloatTensor(train_data_obj.data['X']).permute(0, 3, 1, 2)
+    X_train = torch.FloatTensor(train_data_obj.data['X']).reshape(-1, 3, 32, 32)
     y_train = torch.LongTensor(train_data_obj.data['y'])
-    X_test = torch.FloatTensor(test_data_obj.data['X']).permute(0, 3, 1, 2)
+    X_test = torch.FloatTensor(test_data_obj.data['X']).reshape(-1, 3, 32, 32)
     y_test = torch.LongTensor(test_data_obj.data['y'])
 
     # GPU
